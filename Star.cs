d29282d6e0fda1017fe43d182fc56a83204758fe
OkForg97 <okfrog97@yandex.ru>
2020-03-20 07:11:54 +0500

    PSC | add project start files and classes Star and Controller

---
+++ Star.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanetarySystemsComposer
+{
+    class Star
+    {
+        String Name;
+
+        public Star ()
+        {
+            Name = "Sun";
+        }
+
+        public Star (String name)
+        {
+            Name = name;
+        }
+
+        public String GetName ()
+        {
+            return Name;
+        }
+
+
+    }
+}
