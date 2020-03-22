e32376af218664054b25a6ff988571400ccb4678
OkForg97 <okfrog97@yandex.ru>
2020-03-22 16:02:13 +0500

    PSC | add celestai haractrestics to CelestialObject

--- CelestialObject.cs
+++ CelestialObject.cs
@@ -9,12 +9,17 @@ namespace PlanetarySystemsComposer
 {
     abstract class CelestialObject
     {
-        //basic haractristics
-        String Name { get; set; }
+        //basic haracteristics
         int X { get; set; } //X-Axis
         int Y { get; set; } //Y-Axis
         Image Img { get; set; }
         int ImgSize { get; set; }
+
+        //Celestial Objects haracteristics
+        String Name { get; set; }
+        int Mass { get; set; }
+        int SunDistance { get; set; }
+
 
     }
 }
