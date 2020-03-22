4fad04d4f09fd360e10f122da97e0d1b833edb2e
OkForg97 <okfrog97@yandex.ru>
2020-03-22 15:53:03 +0500

    PSC | add basic vars to CelestialObject

--- CelestialObject.cs
+++ CelestialObject.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace PlanetarySystemsComposer
 {
     abstract class CelestialObject
     {
+        //basic haractristics
+        String Name { get; set; }
+        int X { get; set; } //X-Axis
+        int Y { get; set; } //Y-Axis
+        Image Img { get; set; }
+        int ImgSize { get; set; }
+
     }
 }
